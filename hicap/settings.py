7f9855926955e0994eee6d2f4884ccc3c67804a8
shu zOMG chen <shu.chen@freelancedreams.com>
2011-10-08 10:21:55 -1000

    sqlite now tied to BASE_PATH

--- hicap/settings.py
+++ hicap/settings.py
@@ -1,4 +1,7 @@
 # Django settings for hicap project.
+import os.path
+
+BASE_PATH = os.path.abspath(os.path.dirname(__file__))
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -12,7 +15,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'hicap.sqlite3',                      # Or path to database file if using sqlite3.
+        'NAME': os.path.join(BASE_PATH, 'hicap.sqlite3'),                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.

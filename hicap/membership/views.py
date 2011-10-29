1f79e6b3722f49e4641edff5f1ec9528537b76dc
shu zOMG chen <shu.chen@freelancedreams.com>
2011-10-28 22:21:10 -1000

    Stub template for frontpage

--- hicap/membership/views.py
+++ hicap/membership/views.py
@@ -1,1 +1,9 @@
 # Create your views here.
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+
+class MemberView(object):
+	@classmethod
+	def frontpage(cls, request):
+		context = {}
+		return render_to_response("membership/frontpage.html", context, context_instance=RequestContext(request))
--- hicap/urls.py
+++ hicap/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls.defaults import patterns, include, url
+from hicap.membership.views import MemberView
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # url(r'^$', 'hicap.views.home', name='home'),
-    # url(r'^hicap/', include('hicap.foo.urls')),
+    url(r'^$', MemberView.frontpage, name='home'),
     url(r'^admin/', include(admin.site.urls)),
 )

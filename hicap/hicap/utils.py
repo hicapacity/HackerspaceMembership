f17dafeaac91fbf0e776027f5861074e47269050
shu zOMG chen <shu.chen@freelancedreams.com>
2012-07-15 22:31:18 -1000

    Added attrdict

--- hicap/hicap/utils.py
+++ hicap/hicap/utils.py
@@ -3,3 +3,10 @@ import datetime
 
 def to_unixtime(d):
 	return int(time.mktime(d.timetuple()))
+
+class AttrDict(dict):
+	def __getattr__(self, key):
+		return self[key]
+
+	def __setattr__(self, key, value):
+		self[key] = value
